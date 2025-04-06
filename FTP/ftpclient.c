15b350c2c4f9b2ea0b9f2974e08fa06cb830a3bc
Ashwin K N <ashwinkn2004@gmail.com>
2025-04-06 19:07:49 +0530

    ftpserver client working code

--- FTP/ftpclient.c
+++ FTP/ftpclient.c
@@ -1,35 +1,31 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <fcntl.h>
-#include <string.h>
-#include <netdb.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netdb.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
 
-main()
-{
-    struct sockaddr_in serveraddr;
-    int clientsock, n, rdret, length;
-    char filename[20], filedata[300];
+void main(){
+    int clientsock, n, f, length;
+    struct sockaddr_in serveraddr, clientaddr;
+    char filename[1000], filedata[1000];
+
     bzero((char *)&serveraddr, sizeof(serveraddr));
-    serveraddr.sin_family = AF_INET;
-    serveraddr.sin_port = htons(3030);
-    serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
     clientsock = socket(AF_INET, SOCK_STREAM, 0);
-    if (connect(clientsock, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0)
-    {
-        printf("\nError:Cannot connect...");
-        exit(0);
-    }
-    printf("Enter the name of the file : ");
+    clientaddr.sin_family = AF_INET;
+    clientaddr.sin_port = htons(2000);
+    clientaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    connect(clientsock, (struct sockaddr *)&clientaddr, sizeof(clientaddr));
+    
+    printf("Enter the file name : ");
     scanf("%s", filename);
     length = strlen(filename);
     write(clientsock, filename, length);
-    rdret = read(clientsock, filedata, 300);
-    printf("\nThe contents of the file: \n\n");
-    printf("%s", filedata);
+    read(clientsock, filedata, 1000);
+    printf("Contents : %s", filedata);
     close(clientsock);
 }
--- FTP/ftpserver.c
+++ FTP/ftpserver.c
@@ -1,37 +1,38 @@
 #include<stdio.h>
 #include<string.h>
-#include<sys/types.h>
-#include<arpa/inet.h>
-#include<netinet/in.h>
-#include<sys/socket.h>
+#include<unistd.h>
 #include<fcntl.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netdb.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
 
 void main(){
-	struct sockaddr_in serveraddr, clientaddr;
-	int n, f, newserversock, serversock;
-	char filename[300], filedata[1000];
-	
-	serversock = socket(AF_INET, SOCK_STREAM, 0);
-	
-	bzero((char *)&serveraddr, sizeof(serveraddr));
-	serveraddr.sin_family = AF_INET;
-	serveraddr.sin_port = htons(2000);
-	serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	
-	bind(serversock, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
-	listen(serversock, 2);
-	
-	while(1){
-		int clientsize = sizeof(clientaddr);
-		newserversock = accept(serversock, (struct sockaddr *)&clientaddr, &clientsize);
-		
-		n = read(newserversock, filename, 300);
-		filename[n] = "\0";
-		f = open(filename, O_RDWR);
-		n = read(f, filedata, 1000);
-		printf("Data = %s\n", filedata);
-		write(newserversock, filedata, n);
-	}
-	close(serversock);
-	close(newserversock);
+    int serversock, newserversock, n, f;
+    struct sockaddr_in serveraddr, clientaddr;
+    char filename[1000], filedata[1000];
+
+    bzero((char *)&serveraddr, sizeof(serveraddr));
+    serversock = socket(AF_INET, SOCK_STREAM, 0);
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(2000);
+    serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    bind(serversock, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+    listen(serversock, 5);
+    while(1){
+        int clientsize = sizeof(clientaddr);
+        newserversock = accept(serversock, (struct sockaddr *)&clientaddr, &clientsize);
+
+        n = read(newserversock, filename, 1000);
+        filename[n] = '\0';
+
+        f = open(filename, O_RDWR);
+        n = read(f, filedata, 1000);
+        printf("Contents : %s", filedata);
+        write(newserversock, filedata, n);
+        close(newserversock);
+    }
+    close(serversock);
 }
